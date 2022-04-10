bc985b8ca4cb99a34ac77685f6c4778e7ae34ae3
Sam-2019 <samtmartey@gmail.com>
2022-04-10 22:41:25 +0100

    imported Topbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import React from "react";
 import "@shopify/polaris/build/esm/styles.css";
-import { Page, Card, Button } from "@shopify/polaris";
+import TopBar from "./components/Topbar";
 
 function App() {
   return (
-    <Page title="Example app">
-      <Card sectioned>
-        <Button onClick={() => alert("Button clicked!")}>Example button</Button>
-      </Card>
-    </Page>
+    <>
+      <TopBar />
+    </>
   );
 }
 
