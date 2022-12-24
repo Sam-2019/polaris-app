b4ff8b0a786771351336f0af8620b420ed5c62ea
Sam-2019 <samtmartey@gmail.com>
2022-12-24 09:24:43 +0000

    new imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "@shopify/polaris/build/esm/styles.css";
 import TopBar from "./components/Topbar";
+import NavigationExample from "components/Navigation";
+import GridExample from "components/GridExample";
 
 function App() {
   return (
     <>
       <TopBar />
+      <NavigationExample/>
+      <GridExample />
     </>
   );
 }
